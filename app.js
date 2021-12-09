51318a93a4bd1015d7d7dbcb2f94216d1cc40995
abdullahkhan44 <86144086+abdullahkhan44@users.noreply.github.com>
2021-12-09 03:25:17 -0800

    'first'

---
+++ app.js
@@ -0,0 +1,118 @@
+// CH # 01
+
+// 1 alert("Welcome to Our site");
+// 2 alert("Error! Please enter a valid password")
+// 3 alert("Welcome to JS Land... <br /> Happy Coding!")
+// 4 alert("Welcome to JS Land")
+//  alert(" Happy Coding!<br /> Prevent This page from creating additonal dialogs ")
+// 5 alert("Hello I can run through my console");
+
+//CH # 02
+
+// 1 var username;
+// 2 var fullName = "Abdullah khan"
+// 3 var message = 'Hello world'
+// alert(message);
+
+// 4 var name = "Jhone Doe"
+//   var age = "15 years old";
+//   var certificate = "Mobile Application Development"
+//  alert(name)
+//  alert(age)
+//  alert(certificate)
+
+// 5 alert("PIZZA <br /> PIZZ <br /> PIZ <br /> Pi<br /> P <br />")
+// 6 var email = "example@gmail.com"
+// alert("My Email address is " + email);
+
+// 7 var book = "A smart way to learn JS"
+// alert(book)
+
+// CH # 03
+
+// 1 var age = 18;
+// alert("I am " + age + "Years old")
+// 3 var bithYear;
+// birthYear = 2003;
+//alert(birhYear);
+
+// 4 var name = prompt("Enter your name")
+//  var Product = prompt("Enter your Product");
+//  var Quantity = prompt("Enter Quantity");
+// document.write(name, Product, Quantity);
+
+//CH # 05
+
+// 1 var a = 3;
+//   var b = 5;
+//   var c = a + b;
+//  alert(c);
+// 2 var a = 3;
+//   var b = 5;
+//   var c = a - b;
+//  alert(c);
+
+// 3 var value;
+//  document.write(value)
+// value = 5;
+//  document.write("Intial Value " + value)
+// value += 1;
+//  document.write("Value after increment" + value)
+// value = 7;
+//  document.write("Value after addition" + value)
+// value -= 1;
+//  document.write(value)
+// value / 3;
+//  document.write("Value after dividing" + value)
+
+// CH # 6-9
+//  var a = 10;
+//  document.write("The value a id " + a)
+//  ++a;
+//  document.write("The value a id " + a)
+//  a++;
+//  document.write("The value a id " + a)
+//  --a;
+//  document.write("The value a id " + a)
+//  a--;
+//  document.write("The value a id " + a)
+//
+//
+// var user = prompt("Enter userName")
+// alert("WELCOME " + user " To Our site")
+
+// CH # 9-11
+
+//  var city = promt("Enter your city name", "Karachi");
+//  if(city === "Karachi"){
+//  alert("Welcome to city of lights")
+//  }
+//
+//  var gender = promt("Enter your gender")
+//  if(gender === 'male'){
+//  alert("Welcome sir")
+//  }
+//  else {
+//  alert("Welcome Ma'am ")
+//  }
+//
+//
+// CH # 12-13
+//
+//  var pass = promt("Enter your password")
+//  var confirmPass = promt("Enter your password")
+//  if(confirmPass === pass){
+//  alert(pass);
+//  }
+//
+//
+// CH # 14-16
+// var number = [1,3,4,5,8,8,9]
+// var boolean = [true, false]
+// var mixed = [1, 3, 4, true, "ahmed"]
+//
+//
+// CH # 17-20
+//  for(var i = 0; i <= 10; i ++ ){
+//  console.log(i)
+//  }
